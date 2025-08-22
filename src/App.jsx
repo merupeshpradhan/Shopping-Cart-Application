385f914043d283359351cc7b12567b9e6cb4956b
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-08-22 11:32:37 +0530

    Implement routing and basic structure for shopping cart application with Home and Cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Cart from "./pages/cart";
+import Header from "./components/header";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchListProducts() {
+    setLoading(true);
+    const res = await fetch(`https://fakestoreapi.com/products`);
+    const data = await res.json();
+
+    if (data) {
+      setLoading(false);
+      setProducts(data);
+    }
+  }
+
+  useEffect(() => {
+    fetchListProducts();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles
+            width={"120"}
+            height={"120"}
+            color="rgb(127,29,29)"
+            visible={true}
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+}
